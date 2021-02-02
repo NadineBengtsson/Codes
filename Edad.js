72f9a84808dc98ef2444b1ef478b3bf085f4a24a
Nadine Bengtsson <nadine.bengtsson@gmail.com>
2021-02-02 20:41:38 -0300

    desafio algoritmo condicional

--- Edad.js
+++ Edad.js
@@ -1,15 +1,28 @@
 let accessAllowed;
 let edad = parseInt(prompt('Cual es tu edad?'));
 
-function MayorEdad(){
-if (edad == "") {
-    alert ("Ingresar tu edad");
-}else {
-    if(edad >18){
-        accessAllowed=true;
-    }else {
-        accessAllowed=false;
+const MayorEdad =() => {
+    if (edad == "") {
+        alert ("Ingresar tu edad");
+    } else {
+        if(edad >18){
+            accessAllowed=true;
+        } else {
+            accessAllowed=false;
+        }
+    }};
+    
+    alert(accessAllowed);
+
+let proyectos = ["Oftanmologia", "Oncologia", "Schizophrenia", "Diabetes"]
+
+    for (let proyecto = 0; proyecto < 5; proyecto++) {
+      if (proyecto === 0) {
+        continue;
+      } 
+      proyecto = proyecto + i;
     }
-}};
+    
+    console.log(proyecto);
             
-alert(accessAllowed);
+
